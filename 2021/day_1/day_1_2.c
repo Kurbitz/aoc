34579068bf4b7fd4290aa14e1be08b7c0222214d
Gustav <gustavkanahols@gmail.com>
2021-12-02 02:34:55 +0100

    Dag 1, del 2

---
+++ 2021/day_1/day_1_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	// if (argc != 2)
+	// 	return 1;
+	argv[1] = "input.txt";
+	FILE *input = fopen(argv[1], "r");
+	int increases = -3, pos = 0, depth, sum = 0, prevSum = 0;
+	int arr[3] = {0};
+	while (fscanf(input, "%d\n", &depth) != EOF)
+	{
+		if (pos == 3)
+			pos = 0;
+		sum -= arr[pos];
+		arr[pos] = depth;
+		sum += arr[pos++];
+		if (sum > prevSum)
+			increases++;
+		prevSum = sum;
+	}
+	printf("%d\n", increases);
+	fclose(input);
+	return 0;
+}
