773b2997ea83ac98d71476aa3ddc6c9a34a75dd8
Gustav Kånåhols <gustavkanahols@gmail.com>
2021-12-03 19:40:46 +0100

    refactor och boilerplate

--- 2021/day_3/day_3_1.c
+++ 2021/day_3/day_3_1.c
@@ -1,33 +1,41 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <math.h>
-int main()
+#define LEN 12
+
+int part1(FILE *input);
+
+int main(int argc, char *argv[])
 {
-	FILE *input = fopen("input.txt", "r");
-	int bitOc[12] = {0}, gamma[12] = {0}, epsilon[12] = {0};
-	int lines = 0, g = 0, e = 0;
-	char line[13];
+	char *fName;
+	if (argc == 2)
+		fName = argv[1];
+	else
+		fName = "input.txt";
+	FILE *input = fopen(fName, "r");
+	if (input == NULL)
+		return 2;
+	
+	printf("%d\n", part1(input));
+
+	return 0;
+}
+
+int part1(FILE *input)
+{
+	int bitOc[LEN] = {0}, lines = 0, g = 0, e = 0;
+	char line[LEN + 1];
 	while (fscanf(input, "%s\n", line) != EOF)
 	{
-		for (int i = 0; i < 12; i++)
+		for (int i = 0; i < LEN; i++)
 			bitOc[i] += line[i] - '0';	
 		lines++;
 	}
-	for (int i = 0; i < 12; i++)
+	for (int i = LEN - 1, j = 0; i >= 0; i--, j++)
 	{
-		if (bitOc[i] >= lines/2)
-			gamma[i] = 1;
+		if (bitOc[i] > lines/2)
+			g += 1*pow(2, j);
 		else
-			epsilon[i] = 1;
+			e += 1*pow(2, j);
 	}
-	for (int i = 11, j = 0; i >= 0; i--, j++)
-	{
-		g += gamma[i]*pow(2,j);
-		e += epsilon[i]*pow(2,j);
-	}
-	
-	printf("%d\n", e*g);
-
-	return 0;
+	return e*g;
 }
