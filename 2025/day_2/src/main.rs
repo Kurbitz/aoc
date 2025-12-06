0b3bdd707fec0bc5bc723ec3038eefa464e0341c
Gustav Kånåhols <gustavkanahols@gmail.com>
2025-12-06 18:53:23 +0100

    Use slices and asserts

--- 2025/day_2/src/main.rs
+++ 2025/day_2/src/main.rs
@@ -1,13 +1,13 @@
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("input.txt").expect("");
-    let (first, second) = solve(input);
+    let input = fs::read_to_string("input.txt").unwrap();
+    let (first, second) = solve(&input);
     println!("Part 1: {}", first);
     println!("Part 1: {}", second);
 }
 
-fn solve(input: String) -> (i64, i64) {
+fn solve(input: &str) -> (i64, i64) {
     let mut sum_part_1 = 0;
     let mut sum_part_2 = 0;
     for ids in input.split_terminator(',') {
@@ -23,148 +23,102 @@ fn solve(input: String) -> (i64, i64) {
 }
 
 fn sum_of_repeats_1(start: i64, end: i64) -> i64 {
-    (start..=end).filter(|x| repeats_1(x.to_string())).sum()
+    (start..=end).filter(|x| repeats_1(&x.to_string())).sum()
 }
 
 fn sum_of_repeats_2(start: i64, end: i64) -> i64 {
-    (start..=end).filter(|x| repeats_2(x.to_string())).sum()
+    (start..=end).filter(|x| repeats_2(&x.to_string())).sum()
 }
 
-fn repeats_1(id: String) -> bool {
+fn repeats_1(id: &str) -> bool {
     let len = id.len();
     if len % 2 != 0 {
         false
     } else {
         let (a, b) = id.split_at(len / 2);
-        *a == *b
+        a == b
     }
 }
 
-fn repeats_2(id: String) -> bool {
+fn repeats_2(id: &str) -> bool {
     let len = id.len();
-    let chunk_sizes = (2..=id.len()).filter(|x| len % x == 0).map(|x| len / x);
-
-    for chunk_size in chunk_sizes {
-        let chars: Vec<char> = id.chars().collect();
-        let chunks: Vec<&[char]> = chars.chunks(chunk_size).collect();
-        let strings: Vec<String> = chunks.iter().map(|x| x.iter().collect()).collect();
-
-        let all_same = strings.windows(2).map(|x| &x[0] == &x[1]).all(|x| x);
-        if all_same {
+    for chunks in (2..=id.len()).filter(|x| len % x == 0) {
+        let chunk_size = len / chunks;
+        let first = &id[0..chunk_size];
+        if (1..chunks).all(|i| &id[i * chunk_size..(i + 1) * chunk_size] == first) {
             return true;
         }
     }
     false
 }
 
+const TEST_INPUT: &str = include_str!("test.txt");
+
 #[cfg(test)]
 #[test]
-fn test_part_1() -> Result<(), i64> {
-    let input = fs::read_to_string("test.txt").expect("");
-
-    let (result, _) = solve(input);
-    match result == 1227775554 {
-        true => Ok(()),
-        false => Err(result),
-    }
+fn test_part_1() {
+    let (result, _) = solve(TEST_INPUT);
+    assert_eq!(result, 1227775554)
 }
 
 #[cfg(test)]
 #[test]
-fn test_part_2() -> Result<(), i64> {
-    let input = fs::read_to_string("test.txt").expect("");
-
-    let (_, result) = solve(input);
-    match result == 4174379265 {
-        true => Ok(()),
-        false => Err(result),
-    }
+fn test_part_2() {
+    let (_, result) = solve(TEST_INPUT);
+    assert_eq!(result, 4174379265);
 }
 
 #[test]
-fn test_repeats_1_11() -> Result<(), bool> {
-    match repeats_1(String::from("11")) {
-        true => Ok(()),
-        false => Err(false),
-    }
+fn test_repeats_1_11() {
+    assert!(repeats_1("11"))
 }
 
 #[test]
-fn test_repeats_1_99() -> Result<(), bool> {
-    match repeats_1(String::from("99")) {
-        true => Ok(()),
-        false => Err(false),
-    }
+fn test_repeats_1_99() {
+    assert!(repeats_1("99"))
 }
 
 #[test]
-fn test_repeats_1_1010() -> Result<(), bool> {
-    match repeats_1(String::from("1010")) {
-        true => Ok(()),
-        false => Err(false),
-    }
+fn test_repeats_1_1010() {
+    assert!(repeats_1("1010"))
 }
 
 #[test]
-fn test_repeats_1_1188511885() -> Result<(), bool> {
-    match repeats_1(String::from("1188511885")) {
-        true => Ok(()),
-        false => Err(false),
-    }
+fn test_repeats_1_1188511885() {
+    assert!(repeats_1("1188511885"))
 }
 
 #[test]
-fn test_repeats_1_222222() -> Result<(), bool> {
-    match repeats_1(String::from("222222")) {
-        true => Ok(()),
-        false => Err(false),
-    }
+fn test_repeats_1_222222() {
+    assert!(repeats_1("222222"))
 }
 
 #[test]
-fn test_repeats_1_1698528() -> Result<(), bool> {
-    match repeats_1(String::from("1698528")) {
-        false => Ok(()),
-        true => Err(false),
-    }
+fn test_repeats_1_1698528() {
+    assert!(!repeats_1("1698528"))
 }
 
 #[test]
-fn test_repeats_1_446446() -> Result<(), bool> {
-    match repeats_1(String::from("446446")) {
-        true => Ok(()),
-        false => Err(false),
-    }
+fn test_repeats_1_446446() {
+    assert!(repeats_1("446446"))
 }
 
 #[test]
-fn test_repeats_1_38593859() -> Result<(), bool> {
-    match repeats_1(String::from("38593859")) {
-        true => Ok(()),
-        false => Err(false),
-    }
+fn test_repeats_1_38593859() {
+    assert!(repeats_1("38593859"))
 }
 
 #[test]
-fn test_repeats_2_38593859() -> Result<(), bool> {
-    match repeats_2(String::from("38593859")) {
-        true => Ok(()),
-        false => Err(false),
-    }
+fn test_repeats_2_38593859() {
+    assert!(repeats_2("38593859"))
 }
 
 #[test]
-fn test_repeats_2_824824824() -> Result<(), bool> {
-    match repeats_2(String::from("824824824")) {
-        true => Ok(()),
-        false => Err(false),
-    }
+fn test_repeats_2_824824824() {
+    assert!(repeats_2("824824824"))
 }
 
 #[test]
-fn test_repeats_2_824824823() -> Result<(), bool> {
-    match repeats_2(String::from("824824823")) {
-        false => Ok(()),
-        true => Err(false),
-    }
+fn test_repeats_2_824824823() {
+    assert!(!repeats_2("824824823"))
 }
